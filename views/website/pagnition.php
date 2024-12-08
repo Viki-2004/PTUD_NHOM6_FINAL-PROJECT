789346f77b17bcd73f3b5cc9eccffe1ecec6fa45
Trần Hương Ly <leetran481@gmail.com>
2024-12-09 00:50:46 +0700

    no message

--- views/website/pagnition.php
+++ views/website/pagnition.php
@@ -3,13 +3,13 @@
 if ($current_page > 3) {
     $first_page = 1;
     ?>
-    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$first_page?>">First</a>
+    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$first_page?>">Trang đầu</a>
 <?php
 }
 
 if ($current_page > 1) {
     $prev_page = $current_page - 1; ?>
-    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$prev_page?>">Prev</a>
+    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$prev_page?>">Trước</a>
 <?php
 }
 
@@ -25,13 +25,13 @@ for ($num = 1; $num <= $totalPages; $num++) {
 
 if ($current_page < $totalPages) {
     $next_page = $current_page + 1; ?>
-    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$next_page?>">Next</a>
+    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$next_page?>">Sau</a>
 <?php
 }
 
 if ($current_page < $totalPages - 3) {
     $end_page = $totalPages; ?>
-    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$end_page?>">Last</a>
+    <a class="page-item" href="?<?=$param?>&per_page=<?=$item_per_page?>&page=<?=$end_page?>">Trang cuối</a>
 <?php 
 }
 ?>
