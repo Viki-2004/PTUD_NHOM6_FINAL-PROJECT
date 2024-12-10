b4cf75276aa64b0487b656208321046e6ad09165
VIKI_LAPTOP\kim29 <kim29042004@gmail.com>
2024-12-10 20:54:40 +0700

    cíu toi trang chủ

--- views/website/news.php
+++ views/website/news.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TRANG SẢN PHẨM</title>
+    <title>TRANG TIN TỨC</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
