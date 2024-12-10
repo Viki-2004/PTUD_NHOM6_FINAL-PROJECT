1ce671d3f362ac18b93f239c97c6003e8ebd573d
Trần Hương Ly <leetran481@gmail.com>
2024-12-11 01:24:14 +0700

    responsive cho trang news

--- assets/js/new.js
+++ assets/js/new.js
@@ -4,6 +4,11 @@ function toggleMenu() {
     navbar.classList.toggle('active'); // Toggle class 'active' để hiển thị/ẩn navbar
 }
 
+function toggleMenu1() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+}
+
 
 
 
