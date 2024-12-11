a47e051a8118e493200efc82215fa1defaac965b
DESKTOP-5OSMRDG\DELL <nhatannh218@gmail.com>
2024-12-11 21:18:00 +0700

    no message

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
 <?php
-<<<<<<< HEAD
     //require "./config/connect.php";
-=======
-    require "./config/connect.php";
-<<<<<<< HEAD
     // include "./config/createdatabase.php";
-    echo "<br>";
+    // echo "<br>";
     // include "./config/insertdata.php";
-=======
->>>>>>> a78ac322ff3f7d3139353f06aa96ebe94d757c3a
-    //include "./config/createdatabase.php";
-    //echo "<br>";
-    //include "./config/insertdata.php";
-<<<<<<< HEAD
-    //echo "<br>";
-    //echo "hole fen"
-=======
->>>>>>> origin/nhatanh
-    echo "<br>";
-    echo "hole fen"
->>>>>>> a78ac322ff3f7d3139353f06aa96ebe94d757c3a
+    // echo "<br>";
+    // echo "hole fen"
 ?>
