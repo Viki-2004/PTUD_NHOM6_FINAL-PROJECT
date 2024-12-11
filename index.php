e5b91d0f81d671a156b3b215ec6d9ee863017a17
VIKI_LAPTOP\kim29 <kim29042004@gmail.com>
2024-12-11 16:40:23 +0700

    đổi cấp cho index

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     // require "./config/connect.php";
     // //include "./config/createdatabase.php";
     // //echo "<br>";
@@ -8,30 +7,4 @@
     // //echo "hole fen"
     include "views/website/trangchu.php";
 ?>
-=======
-<<<<<<< HEAD
-require "./config/connect.php";
-//include "./config/createdatabase.php";
-//echo "<br>";
-//include "./config/insertdata.php";
-//echo "<br>";
-//echo "hole fen"
-=======
-<<<<<<< HEAD
-    require "./config/connect.php";
-=======
-    //require "./config/connect.php";
->>>>>>> origin/main
-    //include "./config/createdatabase.php";
-    //echo "<br>";
-    //include "./config/insertdata.php";
-<<<<<<< HEAD
-    echo "<br>";
-    echo "hole fen"
-=======
-    //echo "<br>";
-    //echo "hole fen"
->>>>>>> origin/main
->>>>>>> a23d99a4563ab32b93d2f19029e52a169fc74cb7
-?>
->>>>>>> origin/main
+
