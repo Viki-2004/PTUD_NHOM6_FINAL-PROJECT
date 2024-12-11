2748c50757ce848be6a5e8e3b2e4aab560cf0f25
VIKI_LAPTOP\kim29 <kim29042004@gmail.com>
2024-12-11 22:13:45 +0700

    update trangchu

--- views/website/trangchu.php
+++ views/website/trangchu.php
@@ -4,45 +4,38 @@
 <head>
     <title>POLIDOLL</title>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/trangchu.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/trangchu_queries.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/trangchu.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/trangchu_queries.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <script src="assets/js/trangchu.js"></script>
+    <script src="../../assets/js/trangchu.js"></script>
 </head>
 
 <body>
 <header class="header">
-<<<<<<< HEAD
-    <?php include "views/website/header.php";
-    include "config/connect.php";
-
-    $trendproducts = mysqli_query($conn, "SELECT * FROM product WHERE trending = 1");
-    $new_products = mysqli_query($conn, "SELECT * FROM product WHERE new_arrival = 1");
-=======
     <?php include "./header.php";
     include "../../config/connect.php";
 
-    $products = mysqli_query($conn, "SELECT * FROM product WHERE trending = 1");
->>>>>>> origin/nhatanh
+    $trendproducts = mysqli_query($conn, "SELECT * FROM product WHERE trending = 1");
+    $new_products = mysqli_query($conn, "SELECT * FROM product WHERE new_arrival = 1");
     ?>
     </header>
     <section class="main-banner">
-        <img src="assets/img/trang chu/ảnh bìa trang chủ.png" alt="Main Banner">
+        <img src="../../assets/img/trang chu/ảnh bìa trang chủ.png" alt="Main Banner">
     </section>
     <section class="shop-category">
         <h2>Danh mục sản phẩm</h2>
         <div class="card-container">
             <div class="category">
-                <img src="assets/img/trang chu/trang chủ - lips.png" alt="Lips" class="Category-image">
+                <img src="../../assets/img/trang chu/trang chủ - lips.png" alt="Lips" class="Category-image">
             </div>
             <div class="category">
-                <img src="assets/img/trang chu/trang chủ - eyes.png" alt="Eyes" class="Category-image">
+                <img src="../../assets/img/trang chu/trang chủ - eyes.png" alt="Eyes" class="Category-image">
             </div>
             <div class="category">
-                <img src="assets/img/trang chu/trang chủ - faces.png" alt="Faces" class="Category-image">
+                <img src="../../assets/img/trang chu/trang chủ - faces.png" alt="Faces" class="Category-image">
             </div>
             <div class="category">
-                <img src="assets/img/trang chu/trang chủ - tools.png" alt="Tools" class="Category-image">
+                <img src="../../assets/img/trang chu/trang chủ - tools.png" alt="Tools" class="Category-image">
             </div>
         </div>
     </section>
@@ -55,15 +48,14 @@
                 <div class="products-carousel-wrapper">
                     <div class="products-carousel">
                             <?php
-<<<<<<< HEAD
                             while($row = mysqli_fetch_array($trendproducts)){                          
                             ?>
                             <!-- Product 1 -->
                             <div class="product-item">
                                 <div class="product-image">
                                     <a href = "chitietsanpham.php?sku=<?=$row["sku"]?>">
-                                    <img src="assets/img/products/<?php echo $row["product_img"] ?>" alt="<?php echo $row["product_img"] ?>" class="default_img">
-                                    <img src="assets/img/products/<?php echo $row["product_hover"] ?>" alt="<?php echo $row["product_hover"] ?> hover" class="hover_img">
+                                    <img src="../../assets/img/products/<?php echo $row["product_img"] ?>" alt="<?php echo $row["product_img"] ?>" class="default_img">
+                                    <img src="../../assets/img/products/<?php echo $row["product_hover"] ?>" alt="<?php echo $row["product_hover"] ?> hover" class="hover_img">
                                     </a>
                                     <button class="nav-buy-now-btn">Mua ngay</button>
                                 </div>
@@ -75,32 +67,11 @@
                     </div>
                 </div>
                 <button class="arrow-btn right-btn" >&#10095;</button>
-=======
-                            while($row = mysqli_fetch_array($products)){                          
-                            ?>
-                        <!-- Product 1 -->
-                        <div class="product-item">
-                            <div class="product-image">
-                            <a href = "chitietsanpham.php?sku=<?=$row["sku"]?>">
-                                <img src="../../assets/img/products/<?php echo $row["product_img"] ?>" alt="<?php echo $row["product_img"] ?>" class="default_img">
-                                <img src="../../assets/img/products/<?php echo $row["product_hover"] ?>" alt="<?php echo $row["product_hover"] ?> hover" class="hover_img">
-                                </a>
-                                <button class="nav-buy-now-btn">Mua ngay</button>
-                            </div>
-                            <p class="product-name"><a href = "chitietsanpham.php?sku=<?=$row["sku"]?>"><?php echo $row["product_name"] ?></a>
-                            </p>
-                            <p class="product-price"><?php echo number_format($row['product_price'], 0, ',', '.'); ?>đ</p>
-                        </div>
-                    </div>
-                </div>
-                <button class="arrow-btn right-btn" onclick="navigateCarousel(1)">&#10095;</button>
-                <?php } ?>                
->>>>>>> origin/nhatanh
             </div>
     
             <!-- Right: Banner -->
             <div class="banner-container">
-                <img src="assets/img/trang chu/trending banner.png" alt="Trending Banner">
+                <img src="../../assets/img/trang chu/trending banner.png" alt="Trending Banner">
             </div>
         </div>
     </section>
@@ -108,7 +79,7 @@
     
     <section class="top-selling-collection">
         <div class="top-selling-img">
-            <img src="assets/img/trang chu/top selling collection.png" alt="Top Selling Collection">
+            <img src="../../assets/img/trang chu/top selling collection.png" alt="Top Selling Collection">
         </div>
         <h2>Sản phẩm mới</h2>
     </section>
@@ -116,7 +87,7 @@
     <section class="nav-section">
         <div class="content-container">
     <div class="nav-container">
-                <button class="nav-btn nav-left-btn" >&#10094;</button>
+                <!-- <button class="nav-btn nav-left-btn" >&#10094;</button> -->
                 <div class="nav-carousel-wrapper">
                     <div class="nav-carousel">
                             <?php
@@ -126,8 +97,8 @@
                             <div class="nav-item">
                                 <div class="nav-image">
                                     <a href = "chitietsanpham.php?sku=<?=$row["sku"]?>">
-                                    <img src="assets/img/products/<?php echo $row["product_img"] ?>" alt="<?php echo $row["product_img"] ?>" class="nav-main-image">
-                                    <img src="assets/img/products/<?php echo $row["product_hover"] ?>" alt="<?php echo $row["product_hover"] ?> hover" class="nav-hover-image">
+                                    <img src="../../assets/img/products/<?php echo $row["product_img"] ?>" alt="<?php echo $row["product_img"] ?>" class="nav-main-image">
+                                    <img src="../../assets/img/products/<?php echo $row["product_hover"] ?>" alt="<?php echo $row["product_hover"] ?> hover" class="nav-hover-image">
                                     </a>
                                     <button class="nav-buy-now-btn">Mua ngay</button>
                                 </div>
@@ -138,7 +109,7 @@
                             <?php } ?>
                     </div>
                 </div>
-                <button class="nav-btn nav-right-btn" >&#10095;</button>
+                <!-- <button class="nav-btn nav-right-btn" >&#10095;</button> -->
             </div>
             </div>
         </section>
@@ -170,11 +141,11 @@
         <div class="carousel-container">
             <div class="carousel-track">
                 <!-- Các ảnh -->
-                <div class="carousel-item"><img src="assets/img/trang chu/1.png" alt="Image 1"></div>
-                <div class="carousel-item"><img src="assets/img/trang chu/2.png" alt="Image 2"></div>
-                <div class="carousel-item"><img src="assets/img/trang chu/5.png" alt="Image 3"></div>
-                <div class="carousel-item"><img src="assets/img/trang chu/7.png" alt="Image 4"></div>
-                <div class="carousel-item"><img src="assets/img/trang chu/6.png" alt="Image 8"></div>
+                <div class="carousel-item"><img src="../../assets/img/trang chu/1.png" alt="Image 1"></div>
+                <div class="carousel-item"><img src="../../assets/img/trang chu/2.png" alt="Image 2"></div>
+                <div class="carousel-item"><img src="../../assets/img/trang chu/5.png" alt="Image 3"></div>
+                <div class="carousel-item"><img src="../../assets/img/trang chu/7.png" alt="Image 4"></div>
+                <div class="carousel-item"><img src="../../assets/img/trang chu/6.png" alt="Image 8"></div>
             </div>
         </div>
         <a href="#" class="view-gallery">Xem Thư Viện</a>
@@ -193,10 +164,4 @@
     <?php include "./footer.php"; ?>
     </footer>
 </body>
-<<<<<<< HEAD
-<footer>
-<?php include "views/website/footer.php"; ?>
-</footer>
-=======
->>>>>>> origin/nhatanh
 </html>
